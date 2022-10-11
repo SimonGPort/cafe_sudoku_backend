529a9dffe6d9a137f455955e73a58c4011cd9ca9
SimonGirPort <simon.giroux.portelance@gmail.com>
2022-10-11 15:07:52 -0400

    setup apollo server graphQL

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,20 @@
-import express from 'express';
-const PORT = 6969;
+const express = require("express");
+import { ApolloServer } from "apollo-server-express";
+const PORT = 4000;
+import { typeDefs } from './query/typeDefs';
+import { resolvers } from './resolvers/resolvers'
 
-
-const app=express()
-
-app.get('/',(req,res)=>{
-res.send('Hello')
-}
-)
-
-app.listen(PORT,()=>console.log('Server Running on '+PORT))
+(async () => {
+    const app = express();
+  
+    const server = new ApolloServer({
+        typeDefs,
+        resolvers
+      });
+      await server.start()
+      server.applyMiddleware({ app });
+  
+    app.listen(PORT, () => {
+      console.log(`express server started on http://localhost:${PORT}${server.graphqlPath}`);
+    });
+  })();
